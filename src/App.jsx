6717c193cfafef28b2f2ade29101895f3c0f19e8
younuskhan1 <md.younuskhan412@gmail.com>
2024-03-04 21:23:48 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,72 @@
+import user from "./assets/Cla_r_Mc_Weeney.png";
+import edit from './assets/edit.png';
+import inbox from './assets/envelope-letter.png';
+import settings from './assets/setting.png';
+import help from './assets/question.png';
+import logout from './assets/log-out-50.png';
+import './App.css';
+import { useState } from 'react';
+import { useRef } from 'react';
+import { useEffect } from 'react';
+import PropTypes from 'prop-types'; // ES6
+
+
+
+function App() {
+
+  const [open, setOpen] = useState(false);
+
+  let menuRef = useRef();
+
+  useEffect(() => {
+    let handler = (e)=>{
+      if(!menuRef.current.contains(e.target)){
+        setOpen(false);
+        console.log(menuRef.current);
+      }      
+    };
+
+    document.addEventListener("mousedown", handler);
+    
+
+    return() =>{
+      document.removeEventListener("mousedown", handler);
+    }
+
+  });
+
+  return (
+    <div className="App">
+      <div className='menu-container' ref={menuRef}>
+        <div className='menu-trigger' onClick={()=>{setOpen(!open)}}>
+          <img src={user}></img>
+        </div>
+
+        <div className={`dropdown-menu ${open? 'active' : 'inactive'}`} >
+          <h3>The Kiet<br/><span>Website Designer</span></h3>
+          <ul>
+            <DropdownItem img = {user} text = {"My Profile"}/>
+            <DropdownItem img = {edit} text = {"Edit Profile"}/>
+            <DropdownItem img = {inbox} text = {"Inbox"}/>
+            <DropdownItem img = {settings} text = {"Settings"}/>
+            <DropdownItem img = {help} text = {"Helps"}/>
+            <DropdownItem img = {logout} text = {"Logout"}/>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function DropdownItem(props){
+  console.log(props);
+  return(
+    <li className = 'dropdownItem'>
+      <img src={props.img}></img>
+      <a> {props.text} </a>
+    </li>
+  );
+}
+
+
+export default App;
